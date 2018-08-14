1d788e914073dcedc9c4a8eb4a52bfadfce84400
Osama Salama <osama.m7@gmail.com>
2018-08-14 16:27:19 +0200

    adding collections names to config

--- config/config.js
+++ config/config.js
@@ -2,6 +2,13 @@ module.exports = {
 	"port": 8686,
 	"mongodb": {
 		"url": "mongodb://localhost:27017/rest-boilerplate-dc",
-		"dbName": "twitterdb03"
+		"dbName": "twitterdb03",
+		"collections" : ["FilteredTweetsFriends01", "LevelAFollowers", "LevelAFriends", "analytics"],
+		"collectionsObject": {
+			FilteredTweetsFriends01:"FilteredTweetsFriends01",
+			LevelAFollowers: "LevelAFollowers",
+			LevelAFriends: "LevelAFriends",
+			analytics: "analytics"
+		}
 	}
 };
