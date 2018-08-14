6e4dd5fcfc6c03b83d096bc9944e876a254d08c7
Osama Salama <osama.m7@gmail.com>
2018-08-14 16:27:00 +0200

    Adding app builder for middlewares

---
+++ app.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const compression = require('compression');
+
+
+const Routes = require('./api/src/tweets-api');
+const app = new express();
+
+let middlewares = [
+	compression(),
+	bodyParser.json(),
+];
+
+app.use(middlewares);
+app.use('/', Routes);
+
+module.exports = app;
