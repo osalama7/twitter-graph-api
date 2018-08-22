7d5870b51af48675f5cd669a406b625ba0bc9d1a
Osama Salama <osama.m7@gmail.com>
2018-08-22 17:28:48 +0200

    adding get hashtags action

--- client/redux/actions/actions.js
+++ client/redux/actions/actions.js
@@ -7,7 +7,19 @@ export function loadTweets () {
 		axios.get('http://localhost:8686/tweets')
 				.then((res) => {
 					let tweets= res.data;
-					dispatch({type:'LOAD_TWEETS', tweets})
+					dispatch({type:'LOAD_TWEETS', tweets});
+				}).catch((err) => {
+			console.log(err)
+		})
+	}
+}
+
+export function loadTopHashtags () {
+	return (dispatch) => {
+		axios.get('http://localhost:8686/tweets/interactions')
+				.then((res) => {
+					let interactions = res.data;
+					dispatch({type:'LOAD_INTERACTIONS', interactions});
 				}).catch((err) => {
 			console.log(err)
 		})
